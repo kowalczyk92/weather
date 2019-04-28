b677234fa46bd768cd5645b2baba3c735e498620
Lukasz Kowalczyk <kowal5-6@o2.pl>
2019-04-28 20:43:39 +0200

    [network] add: weather service

--- app/src/main/java/pl/kowalczyk92/weather/data/network/WeatherService.kt
+++ app/src/main/java/pl/kowalczyk92/weather/data/network/WeatherService.kt
@@ -1,3 +1,16 @@
 package pl.kowalczyk92.weather.data.network
 
-interface WeatherService
+import io.reactivex.Single
+import pl.kowalczyk92.weather.BuildConfig
+import pl.kowalczyk92.weather.data.entity.WeatherForecast
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+
+    @GET("data/2.5/weather")
+    fun getWeatherForecast(
+        @Query("q") cityName: String,
+        @Query("appid") appid: String = BuildConfig.APPID
+    ): Single<WeatherForecast>
+}
